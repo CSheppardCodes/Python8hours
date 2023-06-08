cda49984ddb35a46fc99c3e7be1cfeee442d8c00
CSheppardCodes <78242653+CSheppardCodes@users.noreply.github.com>
2023-06-08 16:25:01 +0000

    formatted string

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-#fundimental datatypes
-# print(2**3)
-# print(5//4)
-# print(5%4)
+name, age = input("what is your name and age\n").split()
 
-#actions
+print(f'Hi {name} you are {age} years old')
+
+weather = "\t It's \"kind of \" sunny \\\n how are you"
+print(weather)
+
+some_value = 5
+some_value += 2
+print(some_value)
+
+iq = 100
+user_age = iq / 5
+
+# # operator precedence
+# print((20-3)+2**2)
+
+# # ()
+# # **
+# # */
+# # +-
+
+# fundimental datatypes
+print(2**3)
+print(5 // 4)
+print(5 % 4)
+
+# actions
 print(round(3.1))
 print(abs(-20))
-
-
-
