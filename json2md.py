61ac4a9da5046876e9a4a1ffff0c5bd8b7fea0eb
CSheppardCodes <78242653+CSheppardCodes@users.noreply.github.com>
2023-06-22 17:25:40 +0000

    json test

--- json2md.py
+++ json2md.py
@@ -1,3 +1,3 @@
 #json2md
-myjsonfil = open('5.json', 'r') 
+myjsonfil = open('https://raw.githubusercontent.com/CSheppardCodes/Python8hours/Replit_python8hours/5.json', 'r') 
 print("we reached this point")
