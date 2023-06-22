7ac2a9529f642dc4d4785f6d40053987e5669ceb
CSheppardCodes <78242653+CSheppardCodes@users.noreply.github.com>
2023-06-22 21:11:59 +0000

    mdutil import, initial md file made

---
+++ Example_markdon.md
@@ -0,0 +1,8 @@
+
+markdown file example
+=====================
+
+# Dallas (5)
+  
+	Floor Priority: 2,1  
+	Max Reservations in Section: 2
--- json2md.py
+++ json2md.py
@@ -1,3 +1,40 @@
 #json2md
-myjsonfil = open('https://raw.githubusercontent.com/CSheppardCodes/Python8hours/Replit_python8hours/5.json', 'r') 
+from mdutils.mdutils import MdUtils
+import json
+#read file
+myjsonfile = open('5.json', 'r')
+jsondata = myjsonfile.read()
+
+#parse
+obj = json.loads(jsondata)
+
+#display json contents
+print(str(obj['id']))
+numfloors = obj['floor_priority']
+print(str(obj['two_bays']))
+bayslist = obj['two_bays']
+print(len(bayslist))
+
+#for i in range(len(bayslist)):
+
+print(str(obj['max_reservations_in_section']))
+
+Location = obj['name'].capitalize() + ' (' + obj['id'] + ')'
+Floor_Priority = '\tFloor Priority: ' + str(numfloors[0]) + ',' + str(
+  numfloors[1])  #make forloop
+Max_Reserve = '\tMax Reservations in Section: ' + str(
+  obj['max_reservations_in_section'])
+#for i in range(len(bayslist)):##sort first if mdutil does not do list sort
+
+mdFile = MdUtils(file_name='Example_markdon', title='markdown file example')
+mdFile.new_header(level=1, title=Location)
+mdFile.new_line(Floor_Priority)
+mdFile.new_line(Max_Reserve)
+
+mdFile.create_md_file()
+
+# #md
+# markdown_string = '# Hello World'
+# html_string = markdown.markdown(markdown_string)
+# print(html_string)
 print("we reached this point")
