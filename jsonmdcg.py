70c084b3673d6de0065f005cb7448079a7df3dd0
CSheppardCodes <78242653+CSheppardCodes@users.noreply.github.com>
2023-06-23 17:28:21 +0000

    file update

--- jsonmdcg.py
+++ jsonmdcg.py
@@ -1,30 +1,23 @@
 import json
-#read file
-myjsonfile = open('5.json', 'r')
-jsondata = myjsonfile.read()
-
-#parse
-jsondata = json.loads(jsondata)
-
 
 def generate_markdown(json_data):
   output = ""
 
   # Extract values from the JSON data
-  i = json_data["id"]
-  fp = json_data["floor_priority"]
-  n = json_data["name"]
-  lis = json_data["two_bays"]
-  mris = json_data["max_reservations_in_section"]
+  id = json_data["id"]
+  floor_priority = json_data["floor_priority"]
+  name = json_data["name"]
+  bays= json_data["two_bays"]
+  max_res = json_data["max_reservations_in_section"]
 
   # Generate the output string
-  output += f" # {n.capitalize()} ({i})\n"
-  output += f"    FP: {', '.join(str(x) for x in fp)}\n"
-  output += f"MRIS: {mris}\n"
+  output += f" # {name.capitalize()} ({id})\n"
+  output += f"    FP: {', '.join(str(x) for x in floor_priority)}\n"
+  output += f"MRIS: {max_res}\n"
   output += "\tlis:\n"
 
   # Generate the lis values with bullet points
-  for num in sorted(lis, reverse=True):
+  for num in sorted(bays, reverse=True):
     output += f"- {num}-{num + 1}\n"
 
   return output
