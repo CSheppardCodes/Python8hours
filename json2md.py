a993ade66d816373d5a9529a978aaed5050cef3d
CSheppardCodes <78242653+CSheppardCodes@users.noreply.github.com>
2023-06-23 15:56:42 +0000

    strbayslist, checkmark

--- json2md.py
+++ json2md.py
@@ -1,4 +1,9 @@
 #json2md
+
+#-second implimentation could be write to txt file then change file extention for ultimate customization, code write time takes longer by ~40%
+#-import shutil
+#-shutil.copyfile("testcopy.txt", "testcopy.md")
+#if inserting in order create const table with all venues alphabetical
 from mdutils.mdutils import MdUtils
 import json
 #read file
@@ -10,30 +15,35 @@ obj = json.loads(jsondata)
 
 #display json contents
 bayslist = obj['two_bays']
-print(bayslist.sort(reverse = True))
+bayslist.sort(reverse=True)#sort desc
 print(bayslist)
 print(type(bayslist))
-print(bayslist)
+
+strBaysList = [f'{num}-{num+1}' for num in bayslist]
+print(strBaysList)
+print(type(strBaysList[0]))
 #add second list befor turning to string
-bayslistSTR = (str(x) for x in bayslist)
-print (bayslistSTR)
+#bayslistSTR = (str(x) for x in bayslist)
+#print(bayslistSTR)
 
-
+#assign strings for md file
 Location = obj['name'].capitalize() + ' (' + obj['id'] + ')'
 numfloors = obj['floor_priority']
+#iterate through each item and put a comma between
 Floor_Priority = ','.join(map(str, numfloors))
 Floor_Priority = '\tFloor Priority: ' + Floor_Priority
-Max_Reserve = '\tMax Reservations in Section: ' + str(obj['max_reservations_in_section'])
+Max_Reserve = '\tMax Reservations in Section: ' + str(
+  obj['max_reservations_in_section'])
 Two_Bays = "\tTwo_Bays:"
 
-
+#make markdown file
 mdFile = MdUtils(file_name='Example_markdon', title='markdown file example')
 mdFile.new_header(level=1, title=Location)
 mdFile.new_line(str(Floor_Priority))
 mdFile.new_line(Max_Reserve)
 mdFile.new_line(Two_Bays)
 #mdFile.new_line(str(bayslist))
-mdFile.new_list(bayslistSTR)
+mdFile.new_checkbox_list(strBaysList)
 mdFile.create_md_file()
 
 # #md
